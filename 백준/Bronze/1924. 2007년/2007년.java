03191dd3a5fa396009a51dc17e1af73233e90483
jd-hyun <98350310+jd-hyun@users.noreply.github.com>
2024-02-07 23:55:05 +0900

    [Bronze I] Title: 2007년, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int temp = y;
+
+        for (int i = 0; i < (x - 1); i++) {
+            temp += arr[i];
+        }
+
+        System.out.println(days[temp % 7]);
+    }
+}
