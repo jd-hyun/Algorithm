769710345f2567852f0ce4fa8acb75fb5026119a
jd-hyun <98350310+jd-hyun@users.noreply.github.com>
2024-06-05 20:16:16 +0900

    [Silver I] Title: 볼 모으기, Time: 420 ms, Memory: 45212 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -9,26 +9,47 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
-        int lastRed = 0;
-        int lastBlue = 0;
+        StringTokenizer st1 = new StringTokenizer(s, "B");
+        StringTokenizer st2 = new StringTokenizer(s, "R");
+        int redCount = 0;
+        int blueCount = 0;
+        int countRedTokens = st1.countTokens();
+        int countBlueTokens = st2.countTokens();
 
-        for (int i = N - 1; i >= 0; i--) {
-            if (s.charAt(i) == 'B') {
-                for (int j = i - 1; j >= 0; j--) {
-                    if (s.charAt(j) == 'R') lastRed++;
-                }
-                break;
-            }
+        if ((countRedTokens == 1 && countBlueTokens == 1) || (countRedTokens == 0 && countBlueTokens == 1) || (countRedTokens == 1 && countBlueTokens == 0)) {
+            System.out.println(0);
+            return;
         }
 
-        for (int i = N - 1; i >= 0; i--) {
-            if (s.charAt(i) == 'R') {
-                for (int j = i - 1; j >= 0; j--) {
-                    if (s.charAt(j) == 'B') lastBlue++;
-                }
-                break;
-            }
+        int[] redBalls = new int[countRedTokens];
+        int[] blueBalls = new int[countBlueTokens];
+
+        for (int i = 0; i < countRedTokens; i++) {
+            redBalls[i] = st1.nextToken().length();
+            redCount += redBalls[i];
         }
-        System.out.println(Math.min(lastRed, lastBlue));
+
+        if (s.charAt(0) == 'R' && s.charAt(N - 1) == 'R') {
+            redCount -= Math.max(redBalls[0], redBalls[countRedTokens - 1]);
+        } else if (s.charAt(0) == 'B' && s.charAt(N - 1) == 'R') {
+            redCount -= redBalls[countRedTokens - 1];
+        } else if (s.charAt(0) == 'R' && s.charAt(N - 1) == 'B') {
+            redCount -= redBalls[0];
+        }
+
+        for (int i = 0; i < countBlueTokens; i++) {
+            blueBalls[i] = st2.nextToken().length();
+            blueCount += blueBalls[i];
+        }
+
+        if (s.charAt(0) == 'B' && s.charAt(N - 1) == 'B') {
+            blueCount -= Math.max(blueBalls[0], blueBalls[countBlueTokens - 1]);
+        } else if (s.charAt(0) == 'R' && s.charAt(N - 1) == 'B') {
+            blueCount -= blueBalls[countBlueTokens - 1];
+        } else if (s.charAt(0) == 'B' && s.charAt(N - 1) == 'R') {
+            blueCount -= blueBalls[0];
+        }
+
+        System.out.println(Math.min(redCount, blueCount));
     }
 }
