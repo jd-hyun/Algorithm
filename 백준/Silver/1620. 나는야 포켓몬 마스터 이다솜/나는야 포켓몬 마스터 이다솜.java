25eb2938dd339a2e7930716db267a9132297aa1c
jd-hyun <98350310+jd-hyun@users.noreply.github.com>
2025-02-15 15:25:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 480 ms, Memory: 49120 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -2,29 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        String[] arr = new String[N];
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
+        String[] split = br.readLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int M = Integer.parseInt(split[1]);
+
+        List<String> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
             String pokemon = br.readLine();
-            hashMap.put(pokemon, i);
-            arr[i] = pokemon;
+            list.add(pokemon);
+            map.put(pokemon, i);
         }
 
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            String problem = br.readLine();
-            try {
-                sb.append(arr[Integer.parseInt(problem) - 1]);
-            } catch (NumberFormatException e) {
-                sb.append(hashMap.get(problem) + 1);
-            } finally {
-                sb.append("\n");
+            String line = br.readLine();
+
+            if (map.containsKey(line)) {
+                sb.append(map.get(line)).append("\n");
+            } else {
+                sb.append(list.get(Integer.parseInt(line) - 1)).append("\n");
             }
         }
 
